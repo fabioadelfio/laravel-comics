c01482c0ccafa2d2a690534c83779d56547df808
fabioadelfio <adelfiofabio07@gmail.com>
2025-08-20 14:50:11 +0200

    BONUS 2

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+        <li><a href="{{ route('comics.characters') }}">Characters</a></li>
+        <li><a class="active" href="{{ route('comics.home') }}">Comics</a></li>
+        <li><a href="{{ route('comics.movies') }}">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+    </ul>
+</nav>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,20 +6,7 @@
                     <img src="img/dc-logo.png" alt="logo-dc-comics">
                 </a>
             </div>
-            <nav>
-                <ul>
-                    <li><a href="{{ route('comics.characters') }}">Characters</a></li>
-                    <li><a class="active" href="{{ route('comics.home') }}">Comics</a></li>
-                    <li><a href="{{ route('comics.movies') }}">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Collectibles</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Shop</a></li>
-                </ul>
-            </nav>
+            @include('components.navigation')
         </div>
     </div>
 </header>
